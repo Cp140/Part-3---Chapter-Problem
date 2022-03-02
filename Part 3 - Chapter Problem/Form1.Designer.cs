fea72e5fb3ce4918b868517842381dfc68b151d3
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-03-02 11:04:22 -0500

    Add project files.

---
+++ Part 3 - Chapter Problem/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Part_3___Chapter_Problem
+{
+    partial class BasicInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblHight = new System.Windows.Forms.Label();
+            this.lblMeasurement = new System.Windows.Forms.Label();
+            this.txtHight = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(15, 94);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(75, 23);
+            this.btnCompute.TabIndex = 0;
+            this.btnCompute.Text = "Click me";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(84, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Enter your name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(102, 6);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(102, 29);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 3;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(12, 32);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(76, 13);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Enter your age";
+            // 
+            // lblHight
+            // 
+            this.lblHight.AutoSize = true;
+            this.lblHight.Location = new System.Drawing.Point(12, 58);
+            this.lblHight.Name = "lblHight";
+            this.lblHight.Size = new System.Drawing.Size(81, 13);
+            this.lblHight.TabIndex = 5;
+            this.lblHight.Text = "Enter your hight";
+            // 
+            // lblMeasurement
+            // 
+            this.lblMeasurement.AutoSize = true;
+            this.lblMeasurement.Location = new System.Drawing.Point(21, 73);
+            this.lblMeasurement.Name = "lblMeasurement";
+            this.lblMeasurement.Size = new System.Drawing.Size(57, 13);
+            this.lblMeasurement.TabIndex = 6;
+            this.lblMeasurement.Text = "(In Meters)";
+            // 
+            // txtHight
+            // 
+            this.txtHight.Location = new System.Drawing.Point(102, 55);
+            this.txtHight.Name = "txtHight";
+            this.txtHight.Size = new System.Drawing.Size(100, 20);
+            this.txtHight.TabIndex = 7;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(99, 99);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(42, 13);
+            this.lblOutput.TabIndex = 8;
+            this.lblOutput.Text = "Answer";
+            // 
+            // BasicInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtHight);
+            this.Controls.Add(this.lblMeasurement);
+            this.Controls.Add(this.lblHight);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnCompute);
+            this.Name = "BasicInput";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblHight;
+        private System.Windows.Forms.Label lblMeasurement;
+        private System.Windows.Forms.TextBox txtHight;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Part 3 - Chapter Problem/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_3___Chapter_Problem
+{
+    public partial class BasicInput : Form
+    {
+        public BasicInput()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            //Button click event
+            string name = txtName.Text;
+            double age = Convert.ToDouble(txtAge.Text);
+            double hight = Convert.ToDouble(txtHight);
+
+        }
+    }
+}
